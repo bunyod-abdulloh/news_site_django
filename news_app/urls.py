ec26588732612c56d7b4838fd40263db24fb0357
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-04 09:54:16 +0500

    Commited

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NewsListView, NewsDetailView
+from .views import news_list, news_detail, homePageView
 
 urlpatterns = [
-    path('all/', NewsListView.as_view(), name='all_news_list'),
-    path('<int:id>/', NewsDetailView.as_view(), name='news_detail_page'),
+    path('', homePageView, name='home_page'),
+    path('news/', news_list, name='all_news_list'),
+    path('news/<int:id>/', news_detail, name='news_detail_page'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -5,40 +5,50 @@ from django.views.generic import ListView, DetailView
 from .models import Category, News
 
 
-# def news_list(request):
-#     news = News.published.all()
-#     context = {
-#         'news_list': news,
-#     }
+def news_list(request):
+    news = News.published.all()
+    context = {
+        'news_list': news,
+    }
+
+    return render(request, 'news/news_list.html', context)
+
+
+def news_detail(request, id):
+    news = get_object_or_404(klass=News, pk=id, status=News.Status.Published)
+    context = {
+        'news': news,
+    }
+    return render(request, 'news/news_detail.html', context)
+
+
+def homePageView(request):
+    news = News.published.all()
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'categories': categories,
+    }
+    return render(request, 'news/index.html', context)
+
+# class NewsListView(ListView):
+#     model = News
+#     template_name = 'news/news_list.html'
+#     context_object_name = 'news_list'
 #
-#     return render(request, 'news/news_list.html', context)
+#     def get_queryset(self):
+#         return News.published.all()  # Bu faqat nashr qilingan yangiliklarni ko'rsatadi
 #
 #
-# def news_detail(request, id):
-#     news = get_object_or_404(klass=News, pk=id, status=News.Status.Published)
-#     context = {
-#         'news': news,
-#     }
-#     return render(request, 'news/news_detail.html', context)
-
-class NewsListView(ListView):
-    model = News
-    template_name = 'news/news_list.html'
-    context_object_name = 'news_list'
-
-    def get_queryset(self):
-        return News.published.all()  # Bu faqat nashr qilingan yangiliklarni ko'rsatadi
-
-
-class NewsDetailView(DetailView):
-    model = News
-    template_name = 'news/news_detail.html'
-    context_object_name = 'news'
-
-    def get_object(self, queryset=None):
-        # Bu faqat nashr qilingan yangilikni olishni ta'minlaydi
-
-        obj = super().get_object(queryset)
-        if obj.status != News.Status.Published:
-            raise Http404("Yangilik topilmadi yoki nashr qilinmagan.")
-        return obj
+# class NewsDetailView(DetailView):
+#     model = News
+#     template_name = 'news/news_detail.html'
+#     context_object_name = 'news'
+#
+#     def get_object(self, queryset=None):
+#         # Bu faqat nashr qilingan yangilikni olishni ta'minlaydi
+#
+#         obj = super().get_object(queryset)
+#         if obj.status != News.Status.Published:
+#             raise Http404("Yangilik topilmadi yoki nashr qilinmagan.")
+#         return obj
