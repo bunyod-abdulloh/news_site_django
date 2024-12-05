90873ad0b43d71e5db090de566ba5de12574ab67
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-05 21:08:19 +0500

    Commited

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, contactPageView
+from .views import news_detail, homePageView, ContactPageView, news_List, HomePageView
 
 urlpatterns = [
-    path('', homePageView, name='home_page'),
-    path('news/', news_list, name='all_news_list'),
+    path('', HomePageView.as_view(), name='home_page'),
+    path('news/', news_List, name='all_news_list'),
     path('news/<int:id>/', news_detail, name='news_detail_page'),
-    path('contact-us/', contactPageView, name='contact_page'),
+    path('contact-us/', ContactPageView.as_view(), name='contact_page'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,12 +1,12 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView
 
 from .models import Category, News
 from .forms import ContactForm
 
 
-def news_list(request):
+def news_List(request):
     news = News.published.all()
     context = {
         'news_list': news,
@@ -24,43 +24,44 @@ def news_detail(request, id):
 
 
 def homePageView(request):
-    news = News.published.all()
+    news_list = News.published.all().order_by('-publish_time')
     categories = Category.objects.all()
     context = {
-        'news': news,
+        'news_list': news_list,
         'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
 
-def contactPageView(request):
-    form = ContactForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return HttpResponse("Bizga habar yuborganingiz uchun tashakkur! Tez orada javob qaytarishga harakat qilamiz!")
-    context = {
-        'form': form,
-    }
-    return render(request, 'news/contact.html', context)
+class HomePageView(TemplateView):
+    template_name = 'news/index.html'
 
-# class NewsListView(ListView):
-#     model = News
-#     template_name = 'news/news_list.html'
-#     context_object_name = 'news_list'
-#
-#     def get_queryset(self):
-#         return News.published.all()  # Bu faqat nashr qilingan yangiliklarni ko'rsatadi
-#
-#
-# class NewsDetailView(DetailView):
-#     model = News
-#     template_name = 'news/news_detail.html'
-#     context_object_name = 'news'
-#
-#     def get_object(self, queryset=None):
-#         # Bu faqat nashr qilingan yangilikni olishni ta'minlaydi
-#
-#         obj = super().get_object(queryset)
-#         if obj.status != News.Status.Published:
-#             raise Http404("Yangilik topilmadi yoki nashr qilinmagan.")
-#         return obj
+    def get(self, request, *args, **kwargs):
+        news_list = News.published.all().order_by('-publish_time')
+        print(news_list)
+        categories = Category.objects.all()
+        context = {
+            'news_list': news_list,
+            'categories': categories,
+        }
+        return self.render_to_response(context)
+
+
+class ContactPageView(TemplateView):
+    template_name = 'news/contact.html'
+
+    def get(self, request, *args, **kwargs):
+        form = ContactForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST)
+        if request.method == 'POST' and form.is_valid():
+            form.save()
+            return HttpResponse(
+                "<h2>Bizga habar yuborganingiz uchun tashakkur! Tez orada javob qaytarishga harakat qilamiz!</h2>")
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
