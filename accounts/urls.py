0a69e67915f6bcd2f856205be47068a6fc855e08
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-10 18:01:28 +0500

    Commited

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import user_logout
+from .views import user_logout, dashboard_views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
-    # path('logout/', user_logout, name='logout'),
+    path('profile/', dashboard_views, name='user_profile'),
 ]
