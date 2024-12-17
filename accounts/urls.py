90ce751633e2ee88cf5d6ae7b7841d32749453d1
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-17 13:14:17 +0500

    Commited

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
     path('profile/', dashboard_views, name='user_profile'),
-    path('password-change/', PasswordChangeView.as_view(success_url='/account/password-change-done.html/'),
+    path('password-change/', PasswordChangeView.as_view(success_url='/account/password-change-done/'),
          name='password_change'),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
