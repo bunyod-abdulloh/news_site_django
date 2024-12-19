6dd7ad928bb02190515d039abc686a7bc9b69f8c
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-19 08:48:23 +0500

    Commited

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from .views import user_logout, dashboard_views
-from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
-    path('profile/', dashboard_views, name='user_profile'),
     path('password-change/', PasswordChangeView.as_view(success_url='/account/password-change-done/'),
          name='password_change'),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('profile/', dashboard_views, name='user_profile'),
 ]
